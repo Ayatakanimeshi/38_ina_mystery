231e90c179b3ac80865f9c810324c75f7f57fda9
Ayatakanimeshi <ayatakathebattler2top@gmail.com>
2023-06-30 06:31:07 +0900

    コントローラーからsignup画面の作成まで(途中)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, confirmation: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
 end
